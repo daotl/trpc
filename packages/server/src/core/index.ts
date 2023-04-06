63a599fdeb843ff15d1219b12bde42fa04cc2ea1
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-04-06 20:11:23 +0800

    feat(server): export types TRPCBuilder and TRPC

--- packages/server/src/core/index.ts
+++ packages/server/src/core/index.ts
@@ -1,23 +1,23 @@
 export type {
-  AnyRouter,
-  ProcedureRecord,
-  ProcedureRouterRecord,
-  CreateRouterInner,
-  Router,
-} from './router';
-export { callProcedure } from './router';
+	AnyRouter,
+	ProcedureRecord,
+	ProcedureRouterRecord,
+	CreateRouterInner,
+	Router,
+} from "./router";
+export { callProcedure } from "./router";
 export type {
-  Procedure,
-  AnyProcedure,
-  AnyQueryProcedure,
-  AnyMutationProcedure,
-  AnySubscriptionProcedure,
-  ProcedureParams,
-  ProcedureArgs,
-  ProcedureOptions,
-} from './procedure';
-export type { inferParser } from './parser';
-export { createInputMiddleware, createOutputMiddleware } from './middleware';
+	Procedure,
+	AnyProcedure,
+	AnyQueryProcedure,
+	AnyMutationProcedure,
+	AnySubscriptionProcedure,
+	ProcedureParams,
+	ProcedureArgs,
+	ProcedureOptions,
+} from "./procedure";
+export type { inferParser } from "./parser";
+export { createInputMiddleware, createOutputMiddleware } from "./middleware";
 
-export { initTRPC } from './initTRPC';
-export * from './types';
+export { initTRPC, type TRPCBuilder, type TRPC } from "./initTRPC";
+export * from "./types";
